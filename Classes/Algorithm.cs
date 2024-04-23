e423ec28066f1458a21202b11c93d25782dcc821
Sameliuk <sameliuk.anastasia@gmail.com>
2024-04-23 16:36:44 +0300

    lab init

---
+++ Classes/Algorithm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Classes
+{
+
+    public class Algorithm
+    {
+        public static int BinarySearchArray(int[] array, int key, out int comparisons)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            comparisons = 0;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                comparisons++;
+
+                if (array[mid] == key)
+                    return mid;
+
+                if (array[mid] < key)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
+            return -1;
+        }
+
+        public static DoublyLinkedListNode BinarySearchDoublyLinkedList(DoublyLinkedList list, int key, out int comparisons)
+        {
+            comparisons = 0;
+            int left = 0;
+            int right = list.Count - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                DoublyLinkedListNode midNode = GetNodeAtIndex(list, mid);
+                comparisons++;
+
+                if (midNode.Value == key)
+                {
+                    return midNode;
+                }
+                else if (midNode.Value < key)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return null;
+        }
+
+        public static DoublyLinkedListNode GetNodeAtIndex(DoublyLinkedList list, int index)
+        {
+            if (index < 0 || index >= list.Count)
+            {
+                throw new IndexOutOfRangeException("Invalid index");
+            }
+
+            DoublyLinkedListNode current = list.Head;
+
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+
+            return current;
+        }
+    }
+
+    public class DoublyLinkedListNode
+    {
+        public int Value { get; set; }
+        public int Index { get; set; }
+        public DoublyLinkedListNode Previous { get; set; }
+        public DoublyLinkedListNode Next { get; set; }
+
+        public DoublyLinkedListNode(int value, int index)
+        {
+            Value = value;
+            Index = index;
+        }
+    }
+
+    public class DoublyLinkedList
+    {
+        public DoublyLinkedListNode Head { get; private set; }
+        public DoublyLinkedListNode Tail { get; private set; }
+        public int Count { get; private set; }
+
+        public void AddLast(int value)
+        {
+            DoublyLinkedListNode newNode = new DoublyLinkedListNode(value, Count);
+
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                newNode.Previous = Tail;
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
+
+            Count++;
+        }
+    }
+}
+
---
+++ Lab7/Program.cs
@@ -0,0 +1,68 @@
+﻿using Classes;
+
+namespace Lab7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = { 100, 1000, 5000, 10000, 20000 };
+
+            foreach (int size in sizes)
+            {
+                int[] array = GenerateRandomArray(size);
+                int key = array[new Random().Next(size)];
+
+                DateTime startTime = DateTime.Now;
+                int comparisons;
+                int result = Algorithm.BinarySearchArray(array, key, out comparisons);
+                DateTime endTime = DateTime.Now;
+                TimeSpan executionTime = endTime - startTime;
+
+                Console.WriteLine($"Array size: {size}, Time: {executionTime.TotalMilliseconds} ms, Comparisons: {comparisons}");
+            }
+
+            foreach (int size in sizes)
+            {
+                DoublyLinkedList list = GenerateRandomDoublyLinkedList(size);
+                int key = new Random().Next(1000); 
+
+                DateTime startTime = DateTime.Now;
+                int comparisons;
+                DoublyLinkedListNode result = Algorithm.BinarySearchDoublyLinkedList(list, key, out comparisons);
+                DateTime endTime = DateTime.Now;
+                TimeSpan executionTime = endTime - startTime;
+
+                Console.WriteLine($"List size: {size}, Time: {executionTime.TotalMilliseconds} ms, Comparisons: {comparisons}");
+            }
+        }
+
+        public static int[] GenerateRandomArray(int size)
+        {
+            int[] array = new int[size];
+            Random rand = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(1000);
+            }
+
+            Array.Sort(array);
+            return array;
+        }
+
+        public static DoublyLinkedList GenerateRandomDoublyLinkedList(int size)
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+            Random rand = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                list.AddLast(rand.Next(1000)); 
+            }
+
+            return list;
+        }
+    }
+    
+}
